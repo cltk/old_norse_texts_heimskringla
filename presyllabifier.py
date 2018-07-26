6ec3b9d27f623fba8b0dea76a16dba6d7533c69b
clemsciences <clemsciences@aol.com>
2018-07-26 23:44:57 +0200

    Presyllabifier, beginning of a syllabified corpus

---
+++ presyllabifier.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+__author__ = ["Clément Besnier <clemsciences@aol.com", ]
+__license__ = "MIT License"
+
+
+def read_syllabified_text(filename):
+    pass
+
+
+def presyllbify_text(filename):
+    pass
