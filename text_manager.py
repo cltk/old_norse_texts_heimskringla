4fcc497bc33ed3a0d5bbc197c047b7f87169ded7
clemsciences <clemsciences@aol.com>
2018-08-04 15:47:41 +0200

    Code to read texts : comments

--- text_manager.py
+++ text_manager.py
@@ -1,4 +1,5 @@
-"""
+"""Code made to read texts of the corpus
+
 This code uses texts from http://heimskringla.no
 
 """
@@ -14,7 +15,19 @@ accepted_formats = ["html", "json", "txt"]
 
 
 def text_extractor(orig_format, dest_format, folder, orig_filenames, dest_filenames, extraction_method,
-                mode="r", encoding="utf8"):
+                   mode="r", encoding="utf8"):
+    """
+    From an HTML file gets
+    :param orig_format: "html"
+    :param dest_format: ["txt", "json"]
+    :param folder:
+    :param orig_filenames:
+    :param dest_filenames:
+    :param extraction_method:
+    :param mode:
+    :param encoding:
+    :return:
+    """
     assert orig_format in ["html"]
     assert dest_format in ["txt", "json"]
     if not os.path.exists(os.path.join(folder, dest_format+"_files")):
@@ -26,20 +39,36 @@ def text_extractor(orig_format, dest_format, folder, orig_filenames, dest_filena
 
 
 def extract_text(data):
+    """
+    Simple use of BeautifulSoup
+    :param data:
+    :return:
+    """
     soup = BeautifulSoup(data, 'html.parser')
     return soup.get_text()
 
 
 class TextLoader:
+    """
+    Simple c
+    """
     def __init__(self, name, extension):
         self.name = name
         self.extension = extension
 
     @staticmethod
     def get_available_names():
+        """
+        Get available texts (forlder)
+        :return:
+        """
         return [name for name in os.listdir(".") if "." not in name]
 
     def load(self):
+        """
+        Gets the selected text
+        :return:
+        """
         try:
             if self.extension in ["txt", "html"]:
                 with codecs.open(os.path.join(self.name, self.extension+"_files", "complete."+self.extension),
@@ -57,9 +86,8 @@ class TextLoader:
 if __name__ == "__main__":
     text_extractor("html", "txt", os.path.join("Sæmundar-Edda", "Atlakviða"), ["complete.html"], ["complete.txt"],
                    extract_text)
-    text_extractor("html", "txt", os.path.join("Sæmundar-Edda"
-                                               , "Hávamál"), ["complete.html"], ["complete.txt"], extract_text)
+    text_extractor("html", "txt", os.path.join("Sæmundar-Edda",
+                                               "Hávamál"), ["complete.html"], ["complete.txt"], extract_text)
     loader = TextLoader(os.path.join("Sæmundar-Edda", "Atlakviða"), "txt")
     print(loader.get_available_names())
     print(loader.load()[:100])
-
