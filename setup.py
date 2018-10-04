f7e78d323dd2352ea6b548490037d8898e4679d4
clement_besnier <clemsciences@aol.com>
2018-10-04 18:02:07 +0200

    For package creation

--- setup.py
+++ setup.py
@@ -13,8 +13,10 @@ setup(
     long_description='',
     name='old_norse_texts_heimskringla',
     packages=find_packages(),
-    url='https://github.com/clemsciences/old_norse_texts_heimskringla',
+    url='https://github.com/cltk/old_norse_texts_heimskringla',
     version='1.2.0',
-    zip_safe=True, install_requires=['bs4', 'cltk', 'nltk']
+    zip_safe=True, install_requires=['bs4', 'cltk', 'nltk'],
     # test_suite='cltk.tests.test_cltk',
+    package_data={"Snorra-Edda": ["Snorra-Edda"],
+                  "Sæmundur-Edda": ["Sæmundur-Edda"]},
 )
