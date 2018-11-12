dae47eea1e91f53db1a603ce76a71062f1483bc6
clemsciences <clemsciences@aol.com>
2018-11-12 23:19:02 +0100

    Introduced a universal tagset and made a method to generate all the possible POS tags

--- pos.py
+++ pos.py
@@ -1,4 +1,10 @@
 """
+POS tags reader for Old Norse texts.
+The tagset is available at http://nlp.cs.ru.is/pdf/Tagset.pdf.
+Some changes were made:
+cc for coordinating conjunctions
+ct for
+
 
 """
 
@@ -431,6 +437,22 @@ class MainPOS:
     verbose[conjunction] = "conjunction"
     verbose[unanalysed] = "unanalysed"
     verbose[punctuation] = "punctuation"
+    verbose[foreign] = "foreign"
+
+    # A universal part-of-speech tagset by Petrov S., Das D., McDonald R.
+
+    universal = defaultdict(str)
+    universal[noun] = "NOUN"
+    universal[adjective] = "ADJ"
+    universal[pronoun] = "PRON"
+    universal[article] = "DET"
+    universal[numeral] = "NUM"
+    universal[verb] = "VERB"
+    universal[adverb] = "ADV"  # in this category, belong ADV, ADP and PRT
+    universal[conjunction] = "CONJ"
+    universal[unanalysed] = "X"
+    universal[foreign] = "X"
+    verbose[punctuation] = "."
 
     @staticmethod
     def apply(tag: str, l_pos: list, value: str):
@@ -555,6 +577,102 @@ class MainPOS:
 
         return value
 
+    @staticmethod
+    def parse_universal(tag):
+        """
+        >>> MainPOS.parse_universal('fakeþ')
+        'PRON'
+        >>> MainPOS.parse_universal('sfg3eþ')
+        'VERB'
+        >>> MainPOS.parse_universal('lvensf')
+        'ADJ'
+        >>> MainPOS.parse_universal('fp1en')
+        'PRON'
+        >>> MainPOS.parse_universal('nkee')
+        'NOUN'
+        >>> MainPOS.parse_universal('sþgken')
+        'VERB'
+        >>> MainPOS.parse_universal('nhfn')
+        'NOUN'
+        >>> MainPOS.parse_universal('nveo')
+        'NOUN'
+
+        :param tag:
+        :return:
+        """
+
+        if tag[0] == MainPOS.noun or tag[0] == MainPOS.adjective or\
+                tag[0] == MainPOS.article or tag[0] == MainPOS.pronoun or \
+                tag[0] == MainPOS.numeral or tag[0] == MainPOS.verb or \
+                tag[0] == MainPOS.conjunction or tag[0] == MainPOS.foreign or \
+                tag[0] == MainPOS.unanalysed or tag[0] == MainPOS.punctuation:
+            return MainPOS.universal[tag[0]]
+
+        elif tag[0] == MainPOS.adverb:
+            if len(tag) == 2:
+                if tag[1] == "a":
+                    return "ADV"
+                elif tag[1] == "u":
+                    return "ADV"
+                elif tag[1] == "o" or tag[1] == "þ" or tag[1] == "e":
+                    return "ADP"
+        return ""
+
+    @staticmethod
+    def generate_all_possible_tags():
+        """
+        TODO correct it because this should not return a list whose length is equal to 1249!
+
+        >>> len(MainPOS.generate_all_possible_tags())
+        1249
+
+        :return: All the possible tags
+        """
+        tags = []
+
+        noun_tags = [MainPOS.noun + gender + number + case for gender in Gender.verbose for number in Number.verbose
+                     for case in Case.verbose]
+
+        noun_tags.extend([tag + pn_tag for tag in noun_tags for pn_tag in ProperNoun.verbose])
+        tags.extend(noun_tags)
+
+        adj_tags = [MainPOS.adjective + gender + number + case + declension + degree for gender in Gender.verbose
+                    for number in Number.verbose for case in Case.verbose for declension in Declension.verbose
+                    for degree in Degree.verbose]
+
+        tags.extend(adj_tags)
+
+        pron_tags = [MainPOS.pronoun + pronoun + person + number + case for pronoun in Pronoun.verbose
+                     for person in Person.verbose for number in Number.verbose for case in Case.verbose]
+        pron_tags.extend([MainPOS.pronoun + pronoun + gender + number + case for pronoun in Pronoun.verbose
+                          for gender in Gender.verbose for number in Number.verbose for case in Case.verbose])
+
+        tags.extend(pron_tags)
+
+        tags.extend([MainPOS.article + gender + number + case for gender in Gender.verbose for number in Number.verbose
+                     for case in Case.verbose])
+
+        tags.extend(
+            [MainPOS.numeral + number_category + gender + number + case for number_category in NumberCategory.verbose
+             for gender in Gender.verbose for number in Number.verbose for case in Case.verbose])
+
+        verb_tags = [MainPOS.verb + mood + voice for mood in Mood.verbose for voice in Voice.verbose]
+        verb_tags.extend([MainPOS.verb + mood + voice + gender + number + case for mood in Mood.verbose
+                          for voice in Voice.verbose for gender in Gender.verbose for number in Number.verbose
+                          for case in Case.verbose])
+        verb_tags.extend(
+            [MainPOS.verb + mood + voice + person + number + tense for mood in Mood.verbose for voice in Voice.verbose
+             for person in Person.verbose for number in Number.verbose for tense in Tense.verbose])
+
+        tags.extend(verb_tags)
+
+        tags.extend(["aa", "au", "ao", "aþ", "ae"])
+        tags.extend(["cn", "ct", "cc"])
+        tags.append(MainPOS.foreign)
+        tags.append(MainPOS.unanalysed)
+        tags.append(MainPOS.punctuation)
+        return tags
+
 
 def parse(tag):
     if len(tag) > 0:
@@ -566,3 +684,5 @@ def parse(tag):
 
 if __name__ == "__main__":
     print(parse("sfg3en"))
+    print(len(MainPOS.generate_all_possible_tags()))
+    print(MainPOS.generate_all_possible_tags())
